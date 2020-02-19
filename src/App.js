3308dee1d2bf47e86aff00a915569e884d994041
Chandler Scott <chandlerscott318@gmail.com>
2020-02-19 16:35:44 -0700

    Set Priority

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ import {
 } from "@material-ui/core";
 import { Link, Route } from "react-router-dom";
 import { auth, db } from "./firebase";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 export function App(props) {
   const [user, setUser] = useState(null);
@@ -51,6 +54,7 @@ export function App(props) {
             updated_tasks.push({
               text: data.text,
               checked: data.checked,
+              priority: data.priority,
               id: doc.id
             });
           });
@@ -96,6 +100,15 @@ export function App(props) {
       .collection("tasks")
       .doc(task_id)
       .update({ checked: checked });
+  };
+
+  const handleChange = (priority, task_id) => {
+    console.log(priority, task_id);
+    db.collection("users")
+      .doc(user.uid)
+      .collection("tasks")
+      .doc(task_id)
+      .update({ priority: priority });
   };
 
   if (!user) {
@@ -146,28 +159,48 @@ export function App(props) {
               const labelId = `checkbox-list-label-${value}`;
 
               return (
-                <ListItem key={value.id}>
-                  <ListItemIcon>
-                    <Checkbox
-                      checked={value.checked}
-                      onChange={(e, checked) => {
-                        handleCheckTask(checked, value.id);
-                      }}
-                      // checked={checked.indexOf(value) !== -1}
-                      inputProps={{ "aria-labelledby": labelId }}
-                    />
-                  </ListItemIcon>
-                  <ListItemText id={labelId} primary={value.text} />
-                  <ListItemSecondaryAction>
-                    <IconButton
-                      onClick={() => {
-                        handleDeleteTask(value.id);
-                      }}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>
+                <div>
+                  <ListItem key={value.id}>
+                    <ListItemIcon>
+                      <Checkbox
+                        checked={value.checked}
+                        onChange={(e, checked) => {
+                          handleCheckTask(checked, value.id);
+                        }}
+                        // checked={checked.indexOf(value) !== -1}
+                        inputProps={{ "aria-labelledby": labelId }}
+                      />
+                    </ListItemIcon>
+                    <ListItemText id={labelId} primary={value.text} />
+                    <ListItemSecondaryAction>
+                      <IconButton
+                        onClick={() => {
+                          handleDeleteTask(value.id);
+                        }}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                      <FormControl>
+                        <InputLabel htmlFor="age-native-simple">
+                          Priority
+                        </InputLabel>
+                        <Select
+                          native
+                          value={value.priority}
+                          onChange={e => {
+                            handleChange(e.target.value, value.id);
+                            console.log(value);
+                          }}
+                        >
+                          <option value={"Low"}>Low</option>
+                          <option value={"Medium"}>Medium</option>
+                          <option value={"High"}>High</option>
+                        </Select>
+                      </FormControl>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                  <div></div>
+                </div>
               );
             })}
           </List>
